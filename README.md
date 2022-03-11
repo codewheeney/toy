9b2565c9caa21c3375a2b172df66880469146ed2
Jim Cassidy <jcassidy@proofpoint.com>
2022-03-11 13:48:37 -0800

    More tests

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Scratch programming place
+
+Word list copyright https://github.com/dwyl/english-words
--- src/test/java/org/example/TestWordFinder.java
+++ src/test/java/org/example/TestWordFinder.java
@@ -50,6 +50,139 @@ public class TestWordFinder {
         }
     }
 
+    @Test
+    public void testWordle137() {
+        String candidates = "qwrtypasghjklzxcvbnm";
+
+        WordFinder wordFinder = new WordFinder(
+            candidates.chars().mapToObj(e -> (char) e).collect(Collectors.toList()),
+            "a_t_r");
+
+        List<String> results = wordFinder.findCandidates();
+
+        assertFalse(results.isEmpty());
+
+        for (String result : results) {
+            System.out.println(result);
+        }
+    }
+
+    @Test
+    public void testWordle138() {
+        String candidates = "qwryupfghjklzxcvbm";
+
+        WordFinder wordFinder = new WordFinder(
+            candidates.chars().mapToObj(e -> (char) e).collect(Collectors.toList()),
+            "_ul_y");
+
+        List<String> results = wordFinder.findCandidates();
+
+        assertFalse(results.isEmpty());
+
+        System.out.printf("There are %d results\n", results.size());
+        for (String result : results) {
+            System.out.println(result);
+        }
+    }
+
+    @Test
+    public void testWordle140() {
+        String candidates = "qwetyopasfgjkzxcvnm";
+
+        WordFinder wordFinder = new WordFinder(
+            candidates.chars().mapToObj(e -> (char) e).collect(Collectors.toList()),
+            "_oast");
+
+        List<String> results = wordFinder.findCandidates();
+
+        assertFalse(results.isEmpty());
+
+        System.out.printf("There are %d results\n", results.size());
+        for (String result : results) {
+            System.out.println(result);
+        }
+    }
+
+    @Test
+    public void testWorldAtch() {
+        String candidates = "qwrtypafhjkzxcvm";
+
+        WordFinder wordFinder = new WordFinder(
+            candidates.chars().mapToObj(e -> (char) e).collect(Collectors.toList()),
+            "_atch");
+
+        List<String> results = wordFinder.findCandidates();
+
+        assertFalse(results.isEmpty());
+
+        System.out.printf("There are %d results\n", results.size());
+        for (String result : results) {
+            System.out.println(result);
+        }
+    }
+
+
+    @Test
+    public void testWorldHomeworkCandidate() {
+        String candidates = "qwertypafghjklzxcvbn";
+
+        WordFinder wordFinder = new WordFinder(
+            candidates.chars().mapToObj(e -> (char) e).collect(Collectors.toList()),
+            "_rea_");
+
+        List<String> results = wordFinder.findCandidates();
+
+        assertFalse(results.isEmpty());
+
+        System.out.printf("There are %d results\n", results.size());
+        for (String result : results) {
+            System.out.println(result);
+        }
+    }
+
+    @Test
+    public void testWordleSolved() {
+        String candidates = "qwryupfghjklzxcvbm";
+        String answer = "puppy";
+
+        WordFinder wordFinder = new WordFinder(
+            candidates.chars().mapToObj(e -> (char) e).collect(Collectors.toList()),
+            answer);
+
+        List<String> results = wordFinder.findCandidates();
+
+        assertEquals(results, List.of(answer));
+    }
+
+    @Test
+    public void testBigWerds() {
+        final String answer = "decussate";
+        String candidates = "qwyuofghjdkzxvb";
+        String template = "_ec_ssate";
+
+        WordFinder wordFinder = new WordFinder(
+            candidates.chars().mapToObj(e -> (char) e).collect(Collectors.toList()),
+            template);
+
+        List<String> results = wordFinder.findCandidates();
+
+        assertTrue(results.contains(answer));
+    }
+
+    @Test
+    public void testAllThreeLetterWords() {
+        String candidates = "abcdefghijklmnopqrstuvwxyz";
+        String template = "___";
+
+        WordFinder wordFinder = new WordFinder(
+            candidates.chars().mapToObj(e -> (char) e).collect(Collectors.toList()),
+            template);
+
+        List<String> results = wordFinder.findCandidates();
+
+        assertFalse(results.isEmpty());
+    }
+
     static final int COUNT = 1000;
 
     @Test
